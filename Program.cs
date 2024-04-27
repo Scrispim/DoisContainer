9099d1567fc792941bdeb7f026f69f048c526d4d
Sander Crispim da Silva <sandercrispim@hotmail.com>
2024-04-27 14:18:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        if (args.Length > 0)
+        {
+            foreach (string arg in args)
+            {
+                runCronJob(arg);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nenhum argumento recebido.");
+        }
+
+        Environment.Exit(0);
+    }
+
+    public static void runCronJob(string argumento) {
+
+        Console.WriteLine($"Passando argumento: {argumento}");
+    }
+
+}
